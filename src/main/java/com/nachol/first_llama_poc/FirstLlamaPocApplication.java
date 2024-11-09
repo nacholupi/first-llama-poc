195c5d8c2e2370f5618bc637cb6bed2350ae1b26
Ignacio Lupi <ignacio.lupi@adevinta.com>
2024-11-09 21:31:44 +0100

    Initial commit first POC AI Cool

---
+++ src/main/java/com/nachol/first_llama_poc/FirstLlamaPocApplication.java
@@ -0,0 +1,13 @@
+package com.nachol.first_llama_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstLlamaPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstLlamaPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nachol/first_llama_poc/FirstLlamaPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nachol.first_llama_poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstLlamaPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
